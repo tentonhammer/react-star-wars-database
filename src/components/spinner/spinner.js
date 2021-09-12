4094fb2fc696350e929ae1c8cc45381446c79360
TenTonHammer <goowernor@gmail.com>
2021-09-12 17:33:09 +1000

    feat: spinner component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,14 @@
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-dual-ball">
+      <div className="ldio">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default Spinner;
