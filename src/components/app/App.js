c687f5811bb22784603de92994d5d94114ddc067
TenTonHammer <goowernor@gmail.com>
2021-09-12 17:29:03 +1000

    feat: app component

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import Header from '../header';
+import RandomPlanet from '../random-planet';
+import ItemList from '../item-list';
+import PersonDetails from '../person-details';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <RandomPlanet />
+
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+        <div className="col-md-6">
+          <PersonDetails />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
