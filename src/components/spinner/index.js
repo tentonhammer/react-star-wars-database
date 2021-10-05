44b4f72b68e11ff11f3d63f2a04e05c64d36e943
TenTonHammer <goowernor@gmail.com>
2021-10-05 22:14:30 +1000

    fix: spinner

--- src/components/spinner/index.js
+++ src/components/spinner/index.js
@@ -1,2 +1,15 @@
-import Spinner from './spinner';
+import './spinner.scss';
+
+const Spinner = () => {
+  return (
+    <div className="spinner">
+      <div className="spinner-dual-ball">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
 export default Spinner;
