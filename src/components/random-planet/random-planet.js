b2ff61a076649845abf36498a452da203aa0e04a
TenTonHammer <goowernor@gmail.com>
2021-10-05 23:24:12 +1000

    fix

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -19,7 +19,7 @@ export default class RandomPlanet extends React.Component {
   }
 
   componentWillUnmount() {
-    clearInterval(this.updatePlanet);
+    clearInterval(this.interval);
   }
 
   onPlanetLoaded = planet => {
