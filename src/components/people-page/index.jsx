451cca42bf299525c786292b7277185ce42f459d
TenTonHammer <goowernor@gmail.com>
2021-10-11 19:56:25 +1000

    ref: people-page refactor

--- src/components/people-page/index.jsx
+++ src/components/people-page/index.jsx
@@ -1,39 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import SwapiService from '../../services/swapi-service';
 import ItemList from '../item-list';
-import PersonDetails from '../person-details';
-import ErrorIndicator from '../error-indicator';
-import SwapiService from "../../services/swapi-service";
+import PersonDetails from '../item-details';
+import ErrorBoundry from '../error-boundry';
+import Row from '../row';
 
-export default class PeoplePage extends React.Component {
+export default class PeoplePage extends Component {
   state = {
     selectedPerson: 5,
-    hasError: false,
   };
 
   swapiService = new SwapiService();
-
-  componentDidCatch(error, errorInfo) {
-    this.setState({ hasError: true });
-  }
 
   handleItemSelect = id => {
     this.setState({ selectedPerson: id });
   };
 
   render() {
-    if (this.state.hasError) {
-      return <ErrorIndicator />;
-    }
     const { selectedPerson } = this.state;
-    return (
-      <div className="row mb2">
-        <div className="col-md-6">
-          <ItemList onItemSelected={this.handleItemSelect} getData={this.swapiService.getAllPersons} />
-        </div>
-        <div className="col-md-6">
-          <PersonDetails personId={selectedPerson} />
-        </div>
-      </div>
+
+    const itemList = (
+      <ItemList
+        onItemSelected={this.handleItemSelect}
+        getData={this.swapiService.getAllPersons}
+      >
+        {i => `${i.name} (${i.birthYear})`}
+      </ItemList>
     );
+    const personDetails = (
+      <ErrorBoundry>
+        <PersonDetails itemId={selectedPerson} />
+      </ErrorBoundry>
+    );
+    return <Row left={itemList} right={personDetails} />;
   }
 }
