893a92bc82d0408b8ec9ab55e6ba14fc2993c99f
TenTonHammer <goowernor@gmail.com>
2021-10-11 19:56:08 +1000

    ref: item-list refactor

--- src/components/item-list/item-list.js
+++ src/components/item-list/item-list.js
@@ -14,18 +14,17 @@ class ItemList extends Component {
     });
   }
 
-  renderItems = () => {
-    const { itemList } = this.state;
-
-    return itemList.map(person => {
-      const { id, name } = person;
+  renderItems = (arr) => {
+    return arr.map(item => {
+      const { id } = item;
+      const label = this.props.children(item);
       return (
         <li
           className="list-group-item"
           key={id}
           onClick={() => this.props.onItemSelected(id)}
         >
-          {name}
+          {label}
         </li>
       );
     });
@@ -38,7 +37,7 @@ class ItemList extends Component {
       return <Spinner />;
     }
 
-    const items = this.renderItems();
+    const items = this.renderItems(itemList);
 
     return <ul className="item-list list-group">{items}</ul>;
   }
