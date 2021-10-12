2fb9fc555aa6520c5b6a10cb48c89c160f6db090
Stepanov Nikita <stepanovne@sovcombank.ru>
2021-10-12 15:13:04 +1000

    ref: remove unused logic

--- src/components/item-details/index.js
+++ src/components/item-details/index.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import './item-details.css';
-import SwapiService from '../../services/swapi-service';
-import Spinner from '../spinner';
 import ErrorButton from '../error-button';
 
 const Record = ({ item, field, label }) => {
@@ -14,11 +12,9 @@ const Record = ({ item, field, label }) => {
 };
 
 class ItemDetails extends React.Component {
-  swapiService = new SwapiService();
   state = {
     item: null,
     image: null,
-    loading: false,
   };
 
   componentDidMount() {
@@ -32,47 +28,37 @@ class ItemDetails extends React.Component {
   }
 
   updateItem() {
-    this.setState({ loading: true });
     const { itemId, getData, getImage } = this.props;
     if (!itemId) {
       return;
     }
     getData(itemId).then(item => {
-      this.setState({ item, image: getImage(item), loading: false });
+      this.setState({ item, image: getImage(item) });
     });
   }
 
   render() {
-    const { item, loading, image } = this.state;
+    const { item, image } = this.state;
 
     if (!item) {
       return <span>Select an item from a list</span>;
     }
 
-    const content = loading ? (
-      <Spinner />
-    ) : (
-      <>
-        <img
-          className='item-image'
-          src={image}
-          alt='Character'
-        />
-
-        <div className='card-body'>
-          <h4>{item.name}</h4>
-          <ul className='list-group list-group-flush'>
-            {React.Children.map(this.props.children, (child, index) => {
-              console.log('child', child);
-              return React.cloneElement(child, {item});
-            })}
-          </ul>
-          <ErrorButton className='m-lg-1' />
-        </div>
-      </>
-    );
-
-    return <div className='item-details card'>{content}</div>;
+    return <div className='item-details card'><img
+      className='item-image'
+      src={image}
+      alt='Character'
+    />
+      <div className='card-body'>
+        <h4>{item.name}</h4>
+        <ul className='list-group list-group-flush'>
+          {React.Children.map(this.props.children, (child, index) => {
+            return React.cloneElement(child, { item });
+          })}
+        </ul>
+        <ErrorButton className='m-lg-1' />
+      </div>
+    </div>;
   }
 }
 
