349483ccdb5268490993ac22e82a2ad465a32cb2
Stepanov Nikita <stepanovne@sovcombank.ru>
2021-10-12 15:58:49 +1000

    ref: HOC composition

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,19 +1,15 @@
 import React, { Component } from 'react';
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import './App.scss';
-import ItemDetails, { Record } from '../item-details';
-import SwapiService from '../../services/swapi-service';
 import ErrorBoundry from '../error-boundry';
 import Row from '../row';
 import { PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList } from '../sw-components';
+import './App.scss';
 
 class App extends Component {
   state = {
     showRandomPlanet: true,
   };
-
-  swapiService = new SwapiService();
 
   toggleRandomPlanet = () => {
     this.setState(state => {
@@ -25,60 +21,22 @@ class App extends Component {
 
   render() {
     const planet = this.state.showRandomPlanet ? <RandomPlanet /> : null;
-    const { getPerson, getStarship, getStarshipImage, getPersonImage } = this.swapiService;
-
-    const itemDetails = <ItemDetails itemId={11} getData={getPerson} getImage={getPersonImage}>
-      <Record field='gender' label='Gender' />
-      <Record field='eyeColor' label='Eye Color' />
-    </ItemDetails>;
-    const starshipDetails = <ItemDetails itemId={5} getData={getStarship} getImage={getStarshipImage}>
-      <Record field='model' label='Model' />
-      <Record field='length' label='Length' />
-      <Record field='costInCredits' label='Cost' />
-    </ItemDetails>;
 
     return (
       <ErrorBoundry>
         <div className='col-md-9 m-auto'>
           <Header />
-          {/*{planet}
-        <button
-          onClick={this.toggleRandomPlanet}
-          className="btn btn-warning btn-lg mb-3"
-        >
-          Toggle Random Planet
-        </button>
-        <ErrorButton className='ml-1' />*/}
-          {/*<PeoplePage />*/}
-          {/*<div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.handleItemSelect} getData={this.swapiService.getAllPlanets} renderItem={(item) => item.name} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>
-        <div className="row mb2">
-          <div className="col-md-6">
-            <ItemList onItemSelected={this.handleItemSelect} getData={this.swapiService.getAllStarships} renderItem={(item) => item.name} />
-          </div>
-          <div className="col-md-6">
-            <PersonDetails personId={this.state.selectedPerson} />
-          </div>
-        </div>*/}
-          <Row left={starshipDetails} right={itemDetails} />
-          <PersonList>
-            {({name}) => <span>{name}</span>}
-          </PersonList>
-          <StarshipList>
-            {({name}) => <span>{name}</span>}
-          </StarshipList>
-          <PlanetList>
-            {({name}) => <span>{name}</span>}
-          </PlanetList>
-          <PersonDetails itemId={11} />
-          <PlanetDetails itemId={11} />
-          <StarshipDetails itemId={11} />
+          {planet}
+          <button
+            onClick={this.toggleRandomPlanet}
+            className='btn btn-warning btn-lg mb-3'
+          >
+            Toggle Random Planet
+          </button>
+
+          <Row left={<PersonList />} right={<PersonDetails itemId={11} />} />
+          <Row left={<PlanetList />} right={<PlanetDetails itemId={11} />} />
+          <Row left={<StarshipList />} right={<StarshipDetails itemId={11} />} />
         </div>
       </ErrorBoundry>
     );
--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,14 +1,27 @@
+import React from 'react';
 import SwapiService from '../../services/swapi-service';
 import { withData } from '../hoc-helpers';
 import ItemList from '../item-list';
 
 const swapiService = new SwapiService();
 
-const {getAllPersons, getAllPlanets, getAllStarships} = swapiService;
+const { getAllPersons, getAllPlanets, getAllStarships } = swapiService;
 
-const PersonList = withData(ItemList, getAllPersons);
-const PlanetList = withData(ItemList, getAllPlanets);
-const StarshipList = withData(ItemList, getAllStarships);
+const withChildFunction = (Wrapped, fn) => {
+  return (props) => {
+    return <Wrapped {...props}>
+      {fn}
+    </Wrapped>;
+  };
+};
+
+const PersonList = withData(withChildFunction(ItemList, ({ name, eyeColor, birthYear }) =>
+  <span>{`${name} (${birthYear}, ${eyeColor})`}</span>), getAllPersons);
+const PlanetList = withData(withChildFunction(ItemList, ({ name, population }) =>
+  <span>{`${name} (${population})`}</span>), getAllPlanets);
+const StarshipList = withData(withChildFunction(ItemList, ({ name, model }) =>
+  <span>{`${name} (${model})`}</span>), getAllStarships);
+
 
 export {
   PersonList, PlanetList, StarshipList,
