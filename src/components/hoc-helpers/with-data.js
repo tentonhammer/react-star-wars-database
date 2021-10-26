0e97e9915d99578e32ece0dbcceb60bb469ab4ea
Stepanov Nikita <stepanovne@sovcombank.ru>
2021-10-26 11:41:33 +1000

    ref: add loading, error to state

--- src/components/hoc-helpers/with-data.js
+++ src/components/hoc-helpers/with-data.js
@@ -1,10 +1,13 @@
 import { Component } from 'react';
 import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
 
 const withData = (View) => {
   return class extends Component {
     state = {
       data: null,
+      loading: true,
+      error: false,
     };
 
     componentDidMount() {
@@ -18,13 +21,20 @@ const withData = (View) => {
     }
 
     update() {
+      this.setState({ loading: true, error: false });
       this.props.getData().then(data => {
-        this.setState({ data });
+        this.setState({ data, loading: false });
+      }).catch(() => {
+        this.setState({ error: true, loading: false });
       });
     }
 
     render() {
-      const { data } = this.state;
+      const { data, loading, error } = this.state;
+
+      if (error) {
+        return  <ErrorIndicator />
+      }
 
       if (!data) {
         return <Spinner />;
